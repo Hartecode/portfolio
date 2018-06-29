45a5cdb3cfcca1a2622ae7bdd66c2bf57f6227a8
Sean Harte <scsharte@gmail.com>
2018-06-29 00:33:35 -0400

    correct scroll error

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
-let $nav_header    = $('.banner'),
-    greeting_height = $('.greeting').outerHeight();
- 
+let $header    = $('.banner'),
+    $nav       = $('nav');
+
+
 
 $('.bio').click( () => {
   $('html, body').animate({ scrollTop: $("#bioBox").offset().top}, 1000);
-  $('nav').removeClass('fullWidth');
+  $nav.removeClass('fullWidth');
 });
 
 $('#project').click( () => {
   $('html, body').animate({ scrollTop: $("#projectBox").offset().top}, 1000);
-  $('nav').removeClass('fullWidth');
+  $nav.removeClass('fullWidth');
 });
 
 $('#opennav').on('click', () => {
-	$('nav').toggleClass('fullWidth');
+	$nav.toggleClass('fullWidth');
 });
 
 function navSlide() {
+  
+  let greeting_height = $('.greeting').outerHeight();
   let scroll_top = $(window).scrollTop();
 
+  if (scroll_top) {
+    $nav.removeClass('fullWidth');
+  }
+
   if (scroll_top >= greeting_height) { // the detection!
-    $nav_header.addClass('is-sticky');
+    $header.addClass('is-sticky');
+    $nav.addClass('is-nav-stircky');
   } else {
-    $nav_header.removeClass('is-sticky');
+    $header.removeClass('is-sticky');
+    $nav.removeClass('is-nav-stircky');
   }
 }
 
